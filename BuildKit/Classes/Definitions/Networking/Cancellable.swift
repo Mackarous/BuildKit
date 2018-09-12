7a8da85f3b1a46c00717f0bca8a253739b1ac572
Andrew Mackarous <a.mackarous@gmail.com>
2018-09-12 16:47:40 -0400

    changed to enum

--- BuildKit/Classes/Definitions/Networking/Cancellable.swift
+++ BuildKit/Classes/Definitions/Networking/Cancellable.swift
@@ -2,15 +2,13 @@ public protocol Cancellable {
     func cancel()
 }
 
-public struct Cancellables {
+public enum Cancellables {
     /// Creates a cancellable that does nothing. A No Operation cancellable
     ///
     /// - Returns: Cancellable that does nothing
     public static func create() -> Cancellable {
         return NopCancellable.noOp
     }
-    
-    private init() { }
 }
 
 fileprivate struct NopCancellable: Cancellable {
