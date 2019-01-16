d713ac4606030d525ac2878d7d34a315cf6bb035
Andrew Mackarous <a.mackarous@gmail.com>
2019-01-15 22:02:48 -0500

    Changed up the default JSONDecoding strategy in NetworkOperation

--- BuildKit/Classes/Definitions/Networking/Network.swift
+++ BuildKit/Classes/Definitions/Networking/Network.swift
@@ -1,3 +1,5 @@
+import os
+
 public protocol Network {
     init(sessionManager: SessionManager)
     @discardableResult func perform<T: NetworkOperation>(operation: T, complete: @escaping (Result<T.Response>) -> Void) -> Cancellable
@@ -6,17 +8,44 @@ public protocol Network {
 public protocol NetworkOperation {
     associatedtype Request
     associatedtype Response
+    associatedtype Decoder = JSONDecoder
     var requiresAuthorization: Bool { get }
     func createRequest() throws -> Request
+    func configure(decoder: Decoder)
     func decode(data: Data) throws -> Response
 }
 
-public extension NetworkOperation where Response: Decodable {
+public extension NetworkOperation {
     var requiresAuthorization: Bool {
         return true
     }
-
-    func decode(data: Data) throws -> Response {
-        return try JSONDecoder().decode(Response.self, from: data)
+    
+    func configure(decoder: Decoder) {
+        
     }
+}
+
+public extension NetworkOperation where Response: Decodable, Decoder == JSONDecoder {
+    func decode(data: Data) throws -> Response {
+        let decoder = JSONDecoder()
+        os_log("BuildKit.NetworkOperation: Created decoder %{public}@", type: .debug, String(describing: decoder))
+        configure(decoder: decoder)
+        os_log("BuildKit.NetworkOperation: Configured decoder %{public}@", type: .debug, String(describing: decoder))
+        do {
+            os_log("BuildKit.NetworkOperation: Attempting to decode DataResponse", type: .debug)
+            let dataResponse = try decoder.decode(DataResponse<Response>.self, from: data)
+            os_log("BuildKit.NetworkOperation: Successfully decoded DataResponse", type: .debug)
+            return dataResponse.data
+        } catch {
+            os_log("BuildKit.NetworkOperation: DataResponse decoding unsuccessful...", type: .error, error.localizedDescription)
+        }
+        os_log("BuildKit.NetworkOperation: Attempting to decode Response", type: .debug)
+        let response = try decoder.decode(Response.self, from: data)
+        os_log("BuildKit.NetworkOperation: Successfully decoded Response", type: .debug)
+        return response
+    }
+}
+
+private struct DataResponse<T: Decodable>: Decodable {
+    let data: T
 }
--- BuildKit/Classes/Implementations/Networking/URLSessionNetwork.swift
+++ BuildKit/Classes/Implementations/Networking/URLSessionNetwork.swift
@@ -21,35 +21,35 @@ public struct URLSessionNetwork: Network {
     public func perform<T>(operation: T, complete: @escaping (Result<T.Response>) -> Void) -> Cancellable where T : NetworkOperation {
         do {
             guard var request = try operation.createRequest() as? URLRequest else { throw Error.invalidRequest }
-            os_log("Created network request %s", type: .debug, request.description)
+            os_log("BuildKit.URLSessionNetwork: Created network request %{public}@", type: .debug, request.debugDescription)
             if operation.requiresAuthorization {
                 request.setValue("Bearer \(sessionManager.currentSession.token)", forHTTPHeaderField: "Authorization")
-                os_log("Request requires authorization %s", type: .debug, request.allHTTPHeaderFields?.description ?? "")
+                os_log("BuildKit.URLSessionNetwork: Request requires authorization %{private}@", type: .debug, request.allHTTPHeaderFields.debugDescription)
             }
-            os_log("Performing network request %s", type: .debug, request.description)
+            os_log("BuildKit.URLSessionNetwork: Performing network request %{public}@", type: .debug, request.description)
             let task = URLSession.shared.dataTask(with: request) { data, _, error in
                 if let data = data {
                     do {
-                        os_log("Request completed successfully... Attempting decode", type: .debug)
+                        os_log("BuildKit.URLSessionNetwork: Request completed successfully... Attempting decode", type: .debug)
                         let result = try operation.decode(data: data)
-                        os_log("Decoding complete %s", type: .debug, (result as? CustomStringConvertible)?.description ?? "")
+                        os_log("BuildKit.URLSessionNetwork: Decoding complete %{private}@", type: .debug, String(describing: result))
                         complete(.success(result))
                     } catch {
-                        os_log("Error while decoding data %s", type: .error, error.localizedDescription)
+                        os_log("BuildKit.URLSessionNetwork: Error while decoding data %{public}@", type: .error, error.localizedDescription)
                         complete(.error(error))
                     }
                 } else if let error = error {
-                    os_log("Request incomplete: %s", type: .error, error.localizedDescription)
+                    os_log("BuildKit.URLSessionNetwork: Request incomplete: %{public}@", type: .error, error.localizedDescription)
                     complete(.error(error))
                 } else {
-                    os_log("Unknown error", type: .error)
+                    os_log("BuildKit.URLSessionNetwork: Unknown error", type: .error)
                     complete(.error(Error.unknown))
                 }
             }
             task.resume()
             return task
         } catch {
-            os_log("Unable to create network request %s", type: .error, error.localizedDescription)
+            os_log("BuildKit.URLSessionNetwork: Unable to create network request %{public}@", type: .error, error.localizedDescription)
             complete(.error(error))
         }
         return Cancellables.create()
