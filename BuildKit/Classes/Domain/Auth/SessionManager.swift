3777050326b24bd22686787a86d2c3bab01b789d
Andrew Mackarous <a.mackarous@gmail.com>
2019-03-12 17:00:31 -0400

    Added optional required extension and cleaned up session manager to no longer require network operations

--- BuildKit/Classes/Domain/Auth/SessionManager.swift
+++ BuildKit/Classes/Domain/Auth/SessionManager.swift
@@ -1,13 +1,4 @@
-//
-//  SessionManager.swift
-//  BuildKit
-//
-//  Created by Andrew Mackarous on 2019-01-10.
-//
-
-import Foundation
-
 public protocol SessionManager {
-    var currentSession: Session! { get }
-    func authorizeSession(with network: Network, idToken: String, completion: @escaping (Result<Session>) -> Void)
+    var currentSession: Session { get }
+    func configure(with session: Session)
 }
